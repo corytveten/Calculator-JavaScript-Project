005729150654e151a4bcab5744623aa89af80334
corytveten <corytveten@gmail.com>
2020-07-03 20:11:59 -0700

    symbol and number variables

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 
-const buttons = document.querySelectorAll('.btn');
+const symbols = document.querySelectorAll('.btn-yellow');
 const screen = document.querySelector('.screen');
-console.log(screen);
+const clear = document.querySelector('.btn-clear');
+const equal = document.querySelector('btn-equal');
+const numbers = document.querySelectorAll('.btn-grey')
+console.log(numbers);
 
-buttons.forEach((button) => {
-  button.addEventListener('click', (e) => {
+clear.addEventListener('click', () => {
+  screen.value = "";
+})
+numbers.forEach((number) => {
+  number.addEventListener('click', (e) => {
     console.log(e.target.innerText);
+    const data = e.target.innerText;
+    screen.value = screen.value + data;
+  })
+})
+
+symbols.forEach((symbol) => {
+  symbol.addEventListener('click', (e) => {
+    const operator = e.target.innerText;
+    if (screen.value !== "") {
+      screen.value = screen.value + operator;
+    }
   })
 })
