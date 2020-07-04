60cb663cb44ad69f2db1d85242d2ce81a30fccc9
corytveten <corytveten@gmail.com>
2020-07-03 19:47:54 -0700

    addeventlistener for calc

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 
+const buttons = document.querySelectorAll('.btn');
+const screen = document.querySelector('.screen');
+console.log(screen);
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    console.log(e.target.innerText);
+  })
+})
