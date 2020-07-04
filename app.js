8a0eeaa648b327207d9c1f73ef6ab3f75c9f3dd2
corytveten <corytveten@gmail.com>
2020-07-04 09:07:20 -0700

    doMath function added to calculator

--- app.js
+++ app.js
@@ -4,16 +4,49 @@ const screen = document.querySelector('.screen');
 const clear = document.querySelector('.btn-clear');
 const equal = document.querySelector('btn-equal');
 const numbers = document.querySelectorAll('.btn-grey')
-console.log(numbers);
+
+
+function doMath(value) {
+    console.log(screen.value);
+    const all = screen.value.split(" ");
+    const firstPart = parseFloat(all[0]);
+    const op = all[1];
+    const secondPart = parseFloat(all[2]);
+    console.log(all)
+    switch(op) {
+      case '+':
+        result = firstPart + secondPart;
+        screen.value = result;
+        break;
+      case '-':
+        result = firstPart - secondPart;
+        screen.value = result;
+        break;
+      case '*':
+        result = firstPart * secondPart;
+        screen.value = result;
+        break;
+      case '/':
+        result = firstPart / secondPart;
+        screen.value = result;
+        break;
+      default:
+
+      break;
+    }
+};
 
 clear.addEventListener('click', () => {
   screen.value = "";
 })
+
 numbers.forEach((number) => {
   number.addEventListener('click', (e) => {
     console.log(e.target.innerText);
     const data = e.target.innerText;
+    if (data !== "=" && data !== "C") {
     screen.value = screen.value + data;
+    }
   })
 })
 
@@ -21,7 +54,13 @@ symbols.forEach((symbol) => {
   symbol.addEventListener('click', (e) => {
     const operator = e.target.innerText;
     if (screen.value !== "") {
-      screen.value = screen.value + operator;
+      screen.value = screen.value + " " + operator + " ";
     }
   })
+})
+
+numbers[11].addEventListener('click', () => {
+  if (screen.value !=="") {
+    doMath(screen.value);
+  }
 })
